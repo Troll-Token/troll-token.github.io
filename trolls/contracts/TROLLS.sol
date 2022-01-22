6f8ee4e360bf98e11e055ca7ddd2904da3ae9b8d
TrollToken <98235414+Troll-Token@users.noreply.github.com>
2022-01-22 22:37:09 +0100

    Update TROLLS.sol

--- trolls/contracts/TROLLS.sol
+++ trolls/contracts/TROLLS.sol
@@ -1,1 +1,6 @@
-pragma
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
